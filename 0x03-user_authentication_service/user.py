b395e098e09434c7512804d835dd3ba7c2ef6ec5
birhanus <birhanushimles716@gmail.com>
2022-08-05 15:57:39 +0000

    fix pycodestyle

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 """user models"""
 
 from sqlalchemy import Column, INteger, String
@@ -16,4 +16,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
