215b89e7a0ac1f6977ccc5c3ba92f35a139141f4
birhanus <birhanushimles716@gmail.com>
2022-08-21 18:57:10 +0000

    import typing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """module for personal data"""
 import re
+from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -10,3 +12,17 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         data = re.sub(f'{a}=.*?{separator}',
                       f'{a}={redaction}{separator}', message)
     return data
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
