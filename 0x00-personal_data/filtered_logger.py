6365c4578f183134192dbe448ef186b1eac21bf5
birhanus <birhanushimles716@gmail.com>
2022-08-21 19:30:29 +0000

    fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,20 +9,6 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """fuction called filter_datum that retun the log message obfuscated"""
     for a in fields:
-        data = re.sub(f'{a}=.*?{separator}',
-                      f'{a}={redaction}{separator}', message)
-    return data
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class"""
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+        message = re.sub(f'{a}=.*?{separator}',
+                         f'{a}={redaction}{separator}', message)
+    return message
