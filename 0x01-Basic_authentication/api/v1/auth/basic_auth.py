d6666fd9ce6a819cf6665745f31bb74295585f14
birhanus <birhanushimles716@gmail.com>
2022-08-26 22:52:04 +0000

    Basic- Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,13 +3,14 @@
 from api.v1.auth.auth import Auth
 from flask import Flask, request
 import re
+import base64
 
 
 class BasicAuth(Auth):
     """Basic auth class"""
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        """return Base64 parht of the Authorizaiton
+        """return Base64 part of the Authorizaiton
            header for A Basic Authentication
         """
         if authorization_header is None:
@@ -20,3 +21,17 @@ class BasicAuth(Auth):
             return authorization_header[6:]
         else:
             return None
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            encoded = base64_authorization_header.encode('utf-8')
+            decoded64 = base64.b64decode(encoded)
+            decoded = decoded64.decode('utf-8')
+        except BaseException:
+            return None
+        return decoded
