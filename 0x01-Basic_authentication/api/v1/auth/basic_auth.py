a5f3830c87ee278d1a6f6e5276fd2de436c5b4d4
birhanus <birhanushimles716@gmail.com>
2022-08-26 21:17:09 +0000

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,21 @@
 """basic auth module"""
 from api.v1.auth.auth import Auth
 from flask import Flask, request
+import re
 
 
 class BasicAuth(Auth):
     """Basic auth class"""
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """return Base64 parht of the Authorizaiton
+           header for A Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if re.search("^Basic ", authorization_header):
+            return authorization_header[6:]
+        else:
+            return None
