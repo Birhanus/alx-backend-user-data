da6c13c1c30d7af7d4020a4a49bbfe096be69136
birhanus <birhanushimles716@gmail.com>
2022-08-26 10:19:18 +0000

    Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,11 +5,15 @@ from typing import List,  TypeVar
 
 
 class Auth:
+    """calss to manage the API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require auth"""
         return False
 
     def authorization_header(self, reuest=None) -> str:
+        """authorization header"""
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """return current user"""
         return None
