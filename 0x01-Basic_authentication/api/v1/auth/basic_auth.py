4061873b0d598e27a26531f50b9175d14a82f1b2
birhanus <birhanushimles716@gmail.com>
2022-08-28 09:30:04 +0000

    Basic - Allow password with:

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -46,15 +46,17 @@ class BasicAuth(Auth):
         """returns the user email and password from the Base64
            decoded value.
         """
-        if decoded_base64_authorization_header is None:
-            return None, None
-        if type(decoded_base64_authorization_header) is not str:
-            return None, None
-        if re.search(":", decoded_base64_authorization_header):
-            name, password = re.split(":", decoded_base64_authorization_header)
-            return name, password
-        else:
-            return None, None
+        if type(decoded_base64_authorization_header) == str:
+            pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+            field_match = re.fullmatch(
+                pattern,
+                decoded_base64_authorization_header.strip(),
+            )
+        if field_match is not None:
+            user = field_match.group('user')
+            password = field_match.group('password')
+            return user, password
+        return None, None
 
     def user_object_from_credentials(self, user_email: str, user_pwd:
                                      str) -> TypeVar('User'):
