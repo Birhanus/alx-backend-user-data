2d7a08c2452389006de26ca37c796824c9197d4f
birhanus <birhanushimles716@gmail.com>
2022-08-29 12:56:42 +0000

    fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,7 +9,7 @@ from flask_cors import (CORS, cross_origin)
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -20,8 +20,6 @@ if auth_type == 'auth':
     auth = Auth()
 if auth_type == 'basic_auth':
     auth = BasicAuth()
-if auth_type == 'session_auth':
-    auth = SessionAuth
 
 
 @app.errorhandler(404)
@@ -57,10 +55,11 @@ def authenticate_user():
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
-            request.current_user = auth.current_user(request)
+            user = auth.current_user(request)
+            request.current_user = user
             if auth_header is None:
                 abort(401)
-            if request.current_user is None:
+            if user is None:
                 abort(403)
 
 
