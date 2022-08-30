3b5a6ab4368ee4dee107c06dbaf5f0bd8ffa9e70
birhanus <birhanushimles716@gmail.com>
2022-08-30 07:05:07 +0000

    session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Session Authentication"""
-from api.v1.auth import Auth
+from .auth import Auth
 
 
 class SessionAuth(Auth):
     """class for session Authentication"""
+    pass
