7974692b4e5bb80feabc5f9e86595966d9eba336
birhanus <birhanushimles716@gmail.com>
2022-08-30 07:43:40 +0000

    add documentation for method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,6 +9,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """Create a Session ID for a user_id"""
         if user_id is None:
             return None
         if isinstance(user_id, str):
